ff0134c5425ffc5c0df0a4e756cc04aa13f86d8a
shivansh <shivanshakb@gmail.com>
2020-12-10 01:26:54 +0530

    CI

---
+++ CI_Practice/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="CI_Practice.MvcApplication" Language="VB" %>
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            bool flag = true;
+            Assert.True(flag);
+        }
+    }
+}
